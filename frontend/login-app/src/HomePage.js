a1d64c3b99e990d1bdbf70d30681043b399e808a
sakshinarvekar <narvekarssakshi@gmail.com>
2025-04-25 17:32:28 -0400

    Updated HomePage.js new

--- frontend/login-app/src/HomePage.js
+++ frontend/login-app/src/HomePage.js
@@ -1,17 +1,3 @@
-// src/HomePage.js
- import React from 'react';
-
- function HomePage() {
-   return (
-     <div style={{ padding: '2rem' }}>
-       <h1>Welcome to the Home Page!</h1>
-       <p>You have successfully logged in.</p>
-     </div>
-   );
- }
-
- export default HomePage;
-
 import React, { useState } from 'react';
 import './App.css';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
