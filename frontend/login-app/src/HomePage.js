71474fa031472b5335c1d276eec567663117be45
rupali559 <rupalivaje99@gmail.com>
2025-04-25 17:31:04 -0400

    updated homepage...

--- frontend/login-app/src/HomePage.js
+++ frontend/login-app/src/HomePage.js
@@ -1,16 +1,16 @@
 // src/HomePage.js
-// import React from 'react';
+ import React from 'react';
 
-// function HomePage() {
-//   return (
-//     <div style={{ padding: '2rem' }}>
-//       <h1>Welcome to the Home Page!</h1>
-//       <p>You have successfully logged in.</p>
-//     </div>
-//   );
-// }
+ function HomePage() {
+   return (
+     <div style={{ padding: '2rem' }}>
+       <h1>Welcome to the Home Page!</h1>
+       <p>You have successfully logged in.</p>
+     </div>
+   );
+ }
 
-// export default HomePage;
+ export default HomePage;
 
 import React, { useState } from 'react';
 import './App.css';
